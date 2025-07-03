ebb510121f1bdd0e41e791128dd7ec0938b13db9
suresh228devops <suresh228devops@gmail.com>
2025-07-03 22:06:55 +0530

    Updated code on upto 60-catalogue

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -86,6 +86,17 @@ module "rabbitmq" {
     vpc_id = local.vpc_id
 }
 
+module "catalogue" {
+    #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/suresh228devops/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+
+    sg_name = "catalogue"
+    sg_description = "for catalogue"
+    vpc_id = local.vpc_id
+}
+
 # bastion accepting connections from my laptop
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
@@ -183,6 +194,7 @@ resource "aws_security_group_rule" "mysql_vpn_ssh" {
   security_group_id = module.mysql.sg_id
 }
 
+
 resource "aws_security_group_rule" "rabbitmq_vpn_ssh" {
   count = length(var.rabbitmq_ports_vpn)
   type              = "ingress"
@@ -191,4 +203,49 @@ resource "aws_security_group_rule" "rabbitmq_vpn_ssh" {
   protocol          = "tcp"
   source_security_group_id = module.vpn.sg_id
   security_group_id = module.rabbitmq.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_backend_alb" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = module.backend_alb.sg_id
+  security_group_id = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_vpn_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_vpn_http" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id = module.vpn.sg_id
+  security_group_id = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "catalogue_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id = module.bastion.sg_id
+  security_group_id = module.catalogue.sg_id
+}
+
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  source_security_group_id = module.catalogue.sg_id
+  security_group_id = module.mongodb.sg_id
 }
---
+++ 60-catalogue/catalogue.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+component=$1
+dnf install ansible -y
+#pip3 install boto3 botocore
+ansible-pull -U https://github.com/suresh228devops/ansible-roboshop-roles.git -e component=$1 main.yaml
